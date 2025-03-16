c771dac09331f2d96caf49057c330692022b3d9e
shayan1080 <shayanmolaei1080@gmail.com>
2025-03-16 21:19:59 +0330

    Add react-router-dom and update ConfirmButton component for responsive design

--- src/components/common/ConfirmButton.tsx
+++ src/components/common/ConfirmButton.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import React, { ReactNode } from 'react';
+import { Button, ButtonProps } from '@mui/material';
+import Box from '@mui/material/Box';
 
-function ConfirmButton ()  {
+interface ConfirmButtonProps extends ButtonProps {
+  children: ReactNode;
+}
+
+function ConfirmButton({ children, ...props }: ConfirmButtonProps) {
   return (
-    <Button
-      variant="contained"
-      sx={{
-        borderRadius: '10px',
-        color: 'white',
-        backgroundColor: '#0A155C',
-        minWidth: '150px',
-        fontSize: '10px',
-        padding: 0,
-        height: '20px',
-        '@media (min-width: 500px)': {
-          minWidth: '319px',
-          fontSize: '22px',
-          height: '40px',
-        },
-        '&:hover': {
-          backgroundColor: '#0A155C', // Darker shade for hover
-        },
-        '&:active': {
-          backgroundColor: '#070F3D', // Even darker shade for active state
-        },
-      }}
-    >
-      ورود
-    </Button>
+    <Box sx={{ maxWidth: { xs: '100%', sm: '500px', md: '700px', lg: '900px' } }}>
+      <Button
+        variant="contained"
+        sx={{
+          borderRadius: { xs: '8px', sm: '10px', md: '12px' },
+          color: 'white', 
+          backgroundColor: '#0A155C',
+          padding: 0,
+          minWidth: { xs: '150px', sm: '200px', md: '250px' },
+          fontSize: { xs: '12px', sm: '14px', md: '16px' },
+          height: { xs: '50px', sm: '55px', md: '60px' },
+          '&:hover': {
+            backgroundColor: '#0A155C', // Darker shade for hover
+          },
+          '&:active': {
+            backgroundColor: '#070F3D', // Even darker shade for active state
+          },
+        }}
+        {...props}
+      >
+        {children}
+      </Button>
+    </Box>
   );
-};
+}
 
 export default ConfirmButton;
