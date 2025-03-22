4e7e355db2887896dd9ea43ac05844aa99764134
shayan1080 <shayanmolaei1080@gmail.com>
2025-03-23 02:07:16 +0330

    common confirm button

--- src/components/common/ConfirmButton.tsx
+++ src/components/common/ConfirmButton.tsx
@@ -1,34 +1,49 @@
-import React, { ReactNode } from 'react';
-import { Button, ButtonProps } from '@mui/material';
-import Box from '@mui/material/Box';
+import React from "react";
+import { Button, ButtonProps } from "@mui/material";
+import Box from "@mui/material/Box";
 
 interface ConfirmButtonProps extends ButtonProps {
-  children: ReactNode;
+  name: string;
+  width?: string | number;
+  height?: string | number;
 }
 
-function ConfirmButton({ children, ...props }: ConfirmButtonProps) {
+function ConfirmButton({ name, width = "240px", height = "40px", ...props }: ConfirmButtonProps) {
   return (
-    <Box sx={{ maxWidth: { xs: '100%', sm: '500px', md: '700px', lg: '900px' } }}>
+    <Box display="flex" justifyContent="center">
       <Button
         variant="contained"
         sx={{
-          borderRadius: { xs: '8px', sm: '10px', md: '12px' },
-          color: 'white', 
-          backgroundColor: '#0A155C',
-          padding: 0,
-          minWidth: { xs: '150px', sm: '200px', md: '250px' },
-          fontSize: { xs: '12px', sm: '14px', md: '16px' },
-          height: { xs: '50px', sm: '55px', md: '60px' },
-          '&:hover': {
-            backgroundColor: '#0A155C', // Darker shade for hover
+          width: { xs: "200px", sm: width }, 
+          height: { xs: "35px", sm: height }, 
+          fontSize: { xs: "16px", sm: "20px" } ,
+          textAlign: "center",
+          textTransform: "uppercase",
+          transition: "0.5s",
+          backgroundSize: "200% auto",
+          color: "white",
+          borderRadius: "30px",
+          border: 0,
+          fontWeight: 700,
+          boxShadow: "0px 0px 14px -7px rgb(25, 57, 240)",
+          backgroundImage:
+            "linear-gradient(45deg,rgb(47, 50, 255) 0%,rgb(25, 154, 240) 51%,rgb(1, 7, 98) 100%)",
+          cursor: "pointer",
+          userSelect: "none",
+          WebkitUserSelect: "none",
+          touchAction: "manipulation",
+          "&:hover": {
+            backgroundPosition: "right center",
+            color: "#fff",
+            textDecoration: "none",
           },
-          '&:active': {
-            backgroundColor: '#070F3D', // Even darker shade for active state
+          "&:active": {
+            transform: "scale(0.95)",
           },
         }}
         {...props}
       >
-        {children}
+        {name}
       </Button>
     </Box>
   );
