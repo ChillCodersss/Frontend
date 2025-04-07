b228ac43edc3e0d692162947c1a3d9babceb16dc
Amirazizi192 <118631153+Amirazizi192@users.noreply.github.com>
2025-04-07 14:40:24 +0330

    Fb counselor profile (#10)
    * Common confirm button
    
    * Common Copmponent of InputBox
    
    * Add react-router-dom and update ConfirmButton component for responsive design
    
    * update inputbox prob
    
    * fb-common add toast css file
    
    * resposive common inputbox
    
    * common confirm button
    
    * added image
    
    * fb-Counselor-profile add basic CounselorProfile component with sidebar and profile form
    
    * fb-Counselor-profile add description_text field
    
    * fb-Counselor-profile implement profile image upload and edit functionality in CounselorProfile component
    
    * add SecondaryButton component with customizable styles
    
    * remove boxShadow from SecondaryButton styles
    
    * fb-counselor-profile  add CounselorProfile route and update readOnly properties in CounselorProfile component
    
    * fb-common Enhance InputBox component with new props for readOnly, height, and borderRadius; update styles for improved responsiveness and visual consistency.
    
    * fb-profile-counselor Enhance CounselorProfile component by adding Autocomplete for province selection, updating readOnly properties for form fields, and improving styling for better user experience.  replaced ConfirmButton with SecondaryButton for consistency.
    
    * fb-counselor-profile Refactor CounselorProfile component to improve responsiveness by utilizing useMediaQuery for dynamic styling adjustments based on screen size. Enhanced layout and padding for mobile and tablet view
    
    * fb-counselor-profile Implement province selection with Autocomplete in CounselorProfile component, including API fetching with debounce for improved performance and user experience.
    
    * fb-counselor-profile Update font-family declaration in index.css to use !important for consistency across styles
    
    * Enhance InputBox component in Login page by adding tabIndex and updating borderRadius for improved styling consistency.
    
    * fb-counselor-profile Update border styling in CounselorProfile component for improved visual consistency on hover and focus states.
    
    * Update SecondaryButton component to accept borderRadius prop and apply it in styling; adjust borderColor in CounselorProfile for consistency.
    
    * fb-counselor-profile Add EditIcon for profile image upload in CounselorProfile component and improve layout for better user experience.
    
    * connecting to the back
    
    * fb-counselor-profile edit dropdown search
    
    * fb-counselor-profile apply rtl and right text allign to dropdown options
    
    * fb-counselor-profile update profile image upload icon
    
    * Refactor InputBox component by removing unused tabIndex prop and updating borderRadius for consistent styling; adjust borderRadius in CounselorProfile and Login components for improved UI.
    
    ---------
    
    Co-authored-by: shayan1080 <shayanmolaei1080@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Signup from "./pages/SignUp";
 import Login from "./pages/Login";
+import CounselorProfile from "./pages/CounselorProfile";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/Signup" element={<Signup></Signup>} />
         <Route path="/login" element={<Login></Login>} />
+        <Route path="/CounselorProfile" element={<CounselorProfile></CounselorProfile>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
