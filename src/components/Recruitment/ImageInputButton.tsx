45967b2ce9fba63f3b7dd5e84d82cc4af11a8354
LordM <khalilzadehmm27@gmail.com>
2025-04-17 03:35:35 +0330

    change style of image input button

--- src/components/Recruitment/ImageInputButton.tsx
+++ src/components/Recruitment/ImageInputButton.tsx
@@ -1,9 +1,31 @@
 import { Button } from "@mui/material";
+import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 
-function ImageInputButton() {
+interface Probs {
+    name: string;
+}
+
+function ImageInputButton({ name } : Probs) {
     return (
         <>
-            <Button></Button>
+            <Button
+                disableRipple
+                disableElevation
+                disableFocusRipple
+                variant="outlined"
+                size="large"
+                endIcon={<DriveFolderUploadIcon fontSize="inherit"/>}
+                sx={{
+                    borderRadius: { xs: "6px", sm: "8px", md: "8px" },
+                    border: "2px dashed rgb(204, 207, 209)",
+                    color: "gray",
+                    "&:hover, &.Mui-focusVisible": {
+                        borderStyle: "solid",
+                    },
+                }}
+            > 
+                { name } 
+            </Button>
         </>
     )
 }
