8a5ffd483c3ca94ec5d3628452e90c4d01251538
Seyyed Amirmohammad Mirshamsi <119746684+amirmammad@users.noreply.github.com>
2025-05-07 13:13:57 +0330

    Fix payments (#27)
    * fix(payments): fixed apis and add notifcation component to pay.
    
    * fix(payments): add header
    
    * fix(payments): fix some tested parts
    
    * fix(Payments): add pagination and handle empty payments.
    
    * fix(PaymentsItem): fixed some text cells
    
    * fix(Payment): payment notification with API fixed
    
    * fix(payments): fix parts of payments

--- src/pages/Payments/PaymentsStyle.tsx
+++ src/pages/Payments/PaymentsStyle.tsx
@@ -28,21 +28,15 @@ export const PTableHeadCellStyle: SxProps = {
   padding: "8px",
 };
 
-export const PNotificationBoxStyle: SxProps = {
-  display: "flex",
-  flexDirection: { xs: "column", md: "row" },
-  justifyContent: "center",
-  alignItems: "center",
-  direction: "rtl",
-  margin: "50px 0 20px 0",
-  padding: "10px",
-  backgroundColor: "rgba(90, 129, 235, 0.53)",
-  borderRadius: "8px",
-  border: "1px solid rgba(71, 61, 247)",
-  maxWidth: "75%",
-};
-
-export const PNotificationTextStyle: SxProps = {
-  textAlign: { xs: "center", md: "right" },
-  margin: "10px",
+export const PPaginationStyle: SxProps = {
+  "& .MuiPaginationItem-root": {
+    color: " #057abe",
+    "&.Mui-selected": {
+      backgroundColor: " #057abe",
+      color: "white",
+    },
+    "&.MuiPaginationItem-previousNext": {
+      transform: "rotate(180deg)",
+    },
+  },
 };
