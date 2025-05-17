77b93a5ee0c2f469fa078017cbe54bc2d1bb1d7f
LordM <khalilzadehmm27@gmail.com>
2025-05-18 00:09:40 +0330

    fix errors for build

--- src/pages/CounselorRequests.tsx
+++ src/pages/CounselorRequests.tsx
@@ -261,12 +261,12 @@ const StudentList: React.FC = () => {
     };
   }, [imageUrls, fetchImage, setImageUrls]);
 
-  const handlePageChange = useCallback((event: React.ChangeEvent<unknown>, page: number) => {
+  const handlePageChange = useCallback((_: React.ChangeEvent<unknown>, page: number) => {
     setCurrentPage(page);
   }, []);
 
   const handleMajorFilterChange = useCallback(
-    (event: React.MouseEvent<HTMLElement>, newFilter: string) => {
+    (_: React.MouseEvent<HTMLElement>, newFilter: string) => {
       if (newFilter !== null) {
         setMajorFilter(newFilter);
         setCurrentPage(1);
@@ -276,7 +276,7 @@ const StudentList: React.FC = () => {
   );
 
   const handleGradeFilterChange = useCallback(
-    (event: React.MouseEvent<HTMLElement>, newFilter: string) => {
+    (_: React.MouseEvent<HTMLElement>, newFilter: string) => {
       if (newFilter !== null) {
         setGradeFilter(newFilter);
         setCurrentPage(1);
@@ -286,7 +286,7 @@ const StudentList: React.FC = () => {
   );
 
   const handleStatusFilterChange = useCallback(
-    (event: React.SyntheticEvent, newFilter: string) => {
+    (_: React.SyntheticEvent, newFilter: string) => {
       if (newFilter !== null) {
         setStatusFilter(newFilter);
         setCurrentPage(1);
