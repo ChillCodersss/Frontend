cefa3781ebf6b79b7c4f706d07b80f5dc9aff11c
amirmammad <amr.mrshms83@gmail.com>
2025-06-12 22:29:35 +0330

    feat(ChatService): implement chat functionality with SignalR integration. still .on() functions need some fixes.

---
+++ src/services/chat.ts
@@ -0,0 +1,142 @@
+import * as signalR from "@microsoft/signalr";
+
+const baseURL = "62.60.213.13";
+
+export const getContacts = async (
+  token: string,
+  PageSize: number,
+  PageIndex: number
+) => {
+  const url = new URL(`http://${baseURL}/api/Messages/contacts`);
+  url.searchParams.append("PageSize", PageSize.toString());
+  url.searchParams.append("PageIndex", PageIndex.toString());
+  const response = await fetch(url.toString(), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    console.log("خطا در ارتباط با سرور");
+  }
+
+  return response.json();
+};
+
+export const getMessages = async (token: string, contactId: number) => {
+  const url = new URL(`http://${baseURL}/api/Messages/${contactId}`);
+  const response = await fetch(url.toString(), {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    console.log("خطا در ارتباط با سرور");
+  }
+
+  return response.json();
+};
+
+export class ChatService {
+  private connection: signalR.HubConnection;
+  private jwtToken: string;
+
+  public constructor(token: string) {
+    this.jwtToken = token;
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl(`http://${baseURL}/chat/access_token=${token}`, {})
+      .configureLogging(signalR.LogLevel.Information) // for debugging
+      .build();
+    this.connection.start();
+    this.connection.on("ReceivePrivateMessage", () => {
+      console.log("receiving message need to be handled");
+    });
+
+    this.connection.on("ReceiveOnlineContacts", () => {
+      console.log("need to be handled");
+    });
+
+    this.connection.on("SeenMessage", () => {
+      console.log("we should handle here");
+    });
+
+    this.connection.on("ReceiveUserStatusChange", () => {
+      console.log("User status changing need to be handled");
+    });
+    this.connection.onclose(() => {
+      console.log("Connection closed");
+    });
+  }
+
+  private async uploadMessageFile(token: string, file: File): Promise<string> {
+    const url = new URL(`http://${baseURL}/api/Messages/upload`);
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await fetch(url.toString(), {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+
+    if (!response.ok) {
+      console.log("error in uplaoding file");
+    }
+
+    return String(response);
+  }
+
+  public async sendMessage(message: string, receiverId: string): Promise<void> {
+    try {
+      await this.connection.invoke(
+        "SendPrivateMessage",
+        message,
+        receiverId,
+        false,
+        ""
+      );
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  }
+
+  public async sendFile(file: File, receiverId: string): Promise<void> {
+    const filePath = await this.uploadMessageFile(this.jwtToken, file);
+    try {
+      await this.connection.invoke(
+        "SendPrivateMessage",
+        `${file.name}`,
+        receiverId,
+        true,
+        `${filePath}`
+      );
+    } catch (error) {
+      console.error("Error sending file:", error);
+    }
+  }
+
+  public async openPrivateChat(contactId: number): Promise<boolean> {
+    try {
+      return await this.connection.invoke("OpenPrivateChat", contactId);
+    } catch (error) {
+      console.error("Error opening private chat:", error);
+      return false;
+    }
+  }
+
+  public async closePrivateChat(contactId: number): Promise<boolean> {
+    try {
+      return await this.connection.invoke("ClosePrivateChat", contactId);
+    } catch (error) {
+      console.error("Error closing private chat:", error);
+      return false;
+    }
+  }
+}
