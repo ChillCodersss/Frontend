055058f532c74b07e06b50d92a6f33d442feeebf
LordM <khalilzadehmm27@gmail.com>
2025-06-15 19:59:36 +0330

    feat(main.tsx): added ErrorPage and Services route

--- src/main.tsx
+++ src/main.tsx
@@ -29,6 +29,8 @@ import AboutUs from "./pages/AboutUs/AboutUs";
 import ContactPage from "./pages/Chat/ContactPage";
 import ChatPage from "./pages/Chat/ChatPage";
 import CounselorPayments from "./pages/CounselorPayments/CounselorPayments";
+import Services from "./pages/Services/Services";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 createRoot(document.getElementById("root")!).render(
   <>
@@ -61,6 +63,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/recruitment" element={<Recruitment />} />
           <Route path="/OurCounselor" element={<OurCounselor />} />
           <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
         </Route>
         {/* Dashboard route with nested routes for both roles */}
         <Route path="/dashboard" element={<HSLayout />}>
@@ -86,6 +89,8 @@ createRoot(document.getElementById("root")!).render(
             element={<div>counselor chat</div>}
           />
         </Route>
+
+        <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
     </BrowserRouter>
   </>
